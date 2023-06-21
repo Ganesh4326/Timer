c1e5ccdde7e3d745c0664e0a978027516f0ac743
Ganesh4326 <sgganesh240@gmail.com>
2023-06-21 11:59:18 +0530

    added all

--- src/Timer.js
+++ src/Timer.js
@@ -2,6 +2,7 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import React, { useEffect, useState } from 'react';
 import './timerstyles.css'
+import { FaClock } from "react-icons/fa";
 
 const Timer = () => {
   const [animatedPercentage, setAnimatedPercentage] = useState(100);
@@ -54,7 +55,11 @@ const Timer = () => {
   };
 
   return (
+    <div>
+    <h3 className='h3'>Routine starting in ...</h3>
+    <h5 className='h5'>Subheading here</h5>
     <div style={{ width: '150px', height: '150px', marginLeft:'45%', marginTop:'100px' }}>
+    
       <CircularProgressbar
         value={animatedPercentage}
         text={formatTime(remainingSeconds)}
@@ -66,11 +71,26 @@ const Timer = () => {
           pathTransitionDuration: 0, // Disable path transition
         })}
       />
+
+      
+      </div>
       <div className='btnss'>
       <button className='sub-t1' onClick={handleButtonClick}>+ 10 sec</button>
       <button className='sub-t2' onClick={handleButtonClickD}>- 10 sec</button>
       <button className='sub-t3' onClick={handleButtonClickS}>skip</button>
       </div>
+      <div className="c-main">
+        <h5 className='ch5'>Step <h4 className='ch4'>2</h4>/3</h5>
+        <div className='cleansing'>
+          <img className='img-src' src='https://cdn2.iconfinder.com/data/icons/beauty-salon-makeup-and-cosmetics/64/86-128.png' alt='cosmetic'></img>
+          <div>
+            <h6 className='cl-h6'>Cleansing</h6>
+            <div className='fa-c'><FaClock /><h5> 60 sec</h5></div>
+          </div>
+          <div className="htd"><h5>How to do?</h5></div>
+        </div>
+      </div>
+    
     </div>
   );
 };
